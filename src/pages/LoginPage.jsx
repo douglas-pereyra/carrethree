99d06e2985af28274f63e8a6f48a8bea21d89f90
Douglas da Fontoura Pereyra <dougpereyra@usp.br>
2025-06-18 21:00:49 -0300

    bug fixes

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,5 @@
 // src/pages/LoginPage.jsx
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
 
