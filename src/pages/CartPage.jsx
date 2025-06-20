1fa480cff6c49ff4a698e336eeacc7dfed51d977
Henrique Zucoloto <hzucoloto3@gmail.com>
2025-06-19 21:50:45 -0300

    fix: fixed cart bugs

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -104,7 +104,11 @@ function CartPage() {
 
               <button className="remover" onClick={() => removeItem(item._id)}>
                 <svg xmlns="http://www.w3.org/2000/svg" className="icon-trash" viewBox="0 0 24 24" fill="none" stroke="currentColor">
-                  {/* SVG paths */}
+                  <path d="M3 6h18" strokeWidth="2" strokeLinecap="round"/>
+                  <path d="M7 6v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V6" strokeWidth="2" strokeLinecap="round"/>
+                  <path d="M10 11v4" strokeWidth="2" strokeLinecap="round"/>
+                  <path d="M14 11v4" strokeWidth="2" strokeLinecap="round"/>
+                  <line x1="10" y1="3" x2="14" y2="3" strokeWidth="2" strokeLinecap="round"/>
                 </svg>
               </button>
             </div>
